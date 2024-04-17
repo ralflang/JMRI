805f7a04576b68a56dcc9de5d8b6773120eb3d95
Ralf Lang <ralf.lang01@sap.com>
2024-04-17 11:33:57 +0200

    Add test stub

--- java/test/jmri/jmrix/marklin/MCanReaderTest.java
+++ java/test/jmri/jmrix/marklin/MCanReaderTest.java
@@ -1,2 +1,13 @@
-package jmri.jmrix.marklin;public class MCanReaderTest {
+package jmri.jmrix.marklin;
+
+import static org.junit.jupiter.api.Assertions.assertInstanceOf;
+
+public class MCanReaderTest {
+
+    public void testMcanFromMarklinMessage()
+    {
+        var marklinMessage = MarklinMessage.getEnableMain();
+        var mcanMessage = MCanReader.identify(marklinMessage);
+        assertInstanceOf(MCanMessage.class, mcanMessage);
+    }
 }
